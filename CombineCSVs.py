62781ad6ccd47fa9775e9df72a8892b176cea922
Brian Wilkinson <wilkibrian@gmail.com>
2016-05-22 13:10:57 -0400

    Add comments to clarify splitting lines

--- CombineCSVs.py
+++ CombineCSVs.py
@@ -13,15 +13,19 @@ def combineFiles():
     dirFile = os.path.join(savedir, "combined.csv")
     newFile = open(dirFile, "w")
 
-    newFile.write(startString + "," + "TYPE" +"," + "ADDR" + "\n")
+    newFile.write(startString + "," + "TYPE" +"," + "ADDR" + "\n") # Titles for the first row of spreadsheet.
     for filename in os.listdir(opendir):
-        if filename.endswith(".csv"):
+        if filename.endswith(".csv"): # skip any non-csv files
             pathName = os.path.join(opendir,filename)
             currentFile = open(pathName, "r")
             for line in currentFile:
-                splitLine = line.split(",")
+                splitLine = line.split(",") # create an array of each "word" in the file
+
+                # Make sure the initial cell does not contain REF (a title) or is not blank.
                 if splitLine[0] != startString and splitLine[0] != "":
-                # skip lines that start with REF
+
+                    # since for right now, each row consists of only 3 columns, this is safe to to.
+                    # Note that the third one removes the new line character (the [:-1]
                     newFile.write(splitLine[0] + "," + splitLine[1] + "," + splitLine[2][:-1] + "," + filename + "\n")
             currentFile.close()
     newFile.close()
